9675eae13c01f6d753d68298f4442388b03a98db
Danny <112951958+DannyS193406@users.noreply.github.com>
2023-05-18 17:21:58 +0100

    fences, gates, walls

--- src/main/java/net/itsmythmaster/mccourse/block/ModBlocks.java
+++ src/main/java/net/itsmythmaster/mccourse/block/ModBlocks.java
@@ -66,6 +66,20 @@ public class ModBlocks {
             () -> new PressurePlateBlock(PressurePlateBlock.Sensitivity.EVERYTHING, BlockBehaviour.Properties.of(Material.STONE).strength(3f)
                     .requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
 
+    public static final RegistryObject<Block> COBALT_FENCE = registerBlock("cobalt_fence",
+            () -> new FenceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(3f)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
+
+    public static final RegistryObject<Block> COBALT_FENCE_GATE = registerBlock("cobalt_fence_gate",
+            () -> new FenceGateBlock(BlockBehaviour.Properties.of(Material.STONE).strength(3f)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
+
+    public static final RegistryObject<Block> COBALT_WALL = registerBlock("cobalt_wall",
+            () -> new WallBlock(BlockBehaviour.Properties.of(Material.STONE).strength(3f)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
+
+
+
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block,
                                                                      CreativeModeTab tab, String tooltipKey)
     {
