a0987c066cb3da5bc134e22c8b02be2068b718b8
Danny <112951958+DannyS193406@users.noreply.github.com>
2023-05-18 22:40:54 +0100

    tools

--- src/main/java/net/itsmythmaster/mccourse/item/ModItems.java
+++ src/main/java/net/itsmythmaster/mccourse/item/ModItems.java
@@ -3,8 +3,9 @@ package net.itsmythmaster.mccourse.item;
 import net.itsmythmaster.mccourse.MCCourseMod;
 import net.itsmythmaster.mccourse.item.custom.CoalSliverItem;
 import net.itsmythmaster.mccourse.item.custom.DowsingRodItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.itsmythmaster.mccourse.item.custom.LevitationSwordItem;
+import net.itsmythmaster.mccourse.item.custom.PaxelItem;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -31,6 +32,34 @@ public class ModItems {
 
     public static final RegistryObject<Item> TURNIP = ITEMS.register("turnip",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB).food(ModFoods.TURNIP)));
+
+    public static final RegistryObject<Item> COBALT_SWORD = ITEMS.register("cobalt_sword",
+            () -> new SwordItem(ModTiers.COBALT, 4, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_PICKAXE = ITEMS.register("cobalt_pickaxe",
+            () -> new PickaxeItem(ModTiers.COBALT, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_AXE = ITEMS.register("cobalt_axe",
+            () -> new AxeItem(ModTiers.COBALT, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_SHOVEL= ITEMS.register("cobalt_shovel",
+            () -> new ShovelItem(ModTiers.COBALT, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_HOE = ITEMS.register("cobalt_hoe",
+            () -> new HoeItem(ModTiers.COBALT, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_PAXEL = ITEMS.register("cobalt_paxel",
+            () -> new PaxelItem(ModTiers.COBALT, 4f, 4f,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> LEVITATION_SWORD = ITEMS.register("levitation_sword",
+            () -> new LevitationSwordItem(ModTiers.COBALT, 4, 4f,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
 
     public static void register(IEventBus eventBus)
     {
--- src/main/java/net/itsmythmaster/mccourse/item/ModTiers.java
+++ src/main/java/net/itsmythmaster/mccourse/item/ModTiers.java
@@ -1,4 +1,14 @@
 package net.itsmythmaster.mccourse.item;
 
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+import net.itsmythmaster.mccourse.util.ModTags;
+
 public class ModTiers {
+
+    public static final ForgeTier COBALT = new ForgeTier(5, 2800, 12f,
+            5f, 20, BlockTags.NEEDS_DIAMOND_TOOL,
+            () -> Ingredient.of(ModItems.COBALT_INGOT.get()));
+
 }
