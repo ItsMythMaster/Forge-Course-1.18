3bf0dd8eb5b97520f7b7eedaf8b30304caaa07ca
Danny <112951958+DannyS193406@users.noreply.github.com>
2023-05-19 16:12:01 +0100

    data tablet

--- src/main/java/net/itsmythmaster/mccourse/item/custom/DowsingRodItem.java
+++ src/main/java/net/itsmythmaster/mccourse/item/custom/DowsingRodItem.java
@@ -1,8 +1,11 @@
 package net.itsmythmaster.mccourse.item.custom;
 
+import net.itsmythmaster.mccourse.item.ModItems;
+import net.itsmythmaster.mccourse.util.InventoryUtil;
 import net.itsmythmaster.mccourse.util.ModTags;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
@@ -14,7 +17,6 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -41,6 +43,12 @@ public class DowsingRodItem extends Item {
                 {
                     outputValuableCoordinates(positionClicked.below(i), player, blockBelow);
                     foundBlock = true;
+
+                    if(InventoryUtil.hasPlayerStackInInventory(player, ModItems.DATA_TABLET.get()))
+                    {
+                        addNbtToDataTablet(player, positionClicked.below(i), blockBelow);
+                    }
+
                     break;
                 }
 
@@ -64,6 +72,18 @@ public class DowsingRodItem extends Item {
     {
         player.sendMessage(new TextComponent("Found " + blockBelow.asItem().getRegistryName().toString() + " at " +
                 "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"), player.getUUID());
+    }
+
+    private void addNbtToDataTablet(Player player, BlockPos pos, Block blockBelow)
+    {
+        ItemStack dataTablet =
+                player.getInventory().getItem(InventoryUtil.getFirstInventoryIndex(player, ModItems.DATA_TABLET.get()));
+
+        CompoundTag nbtData = new CompoundTag();
+        nbtData.putString("mccourse.last_ore", "Found " + blockBelow.asItem().getRegistryName().toString() + " at (" +
+                pos.getX() + ", " + pos.getY() + ", " + pos.getZ() + ")");
+
+        dataTablet.setTag(nbtData);
     }
 
     @Override
