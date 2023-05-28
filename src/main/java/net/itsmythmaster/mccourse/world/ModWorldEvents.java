e3f1d1450d519c48c8fc79ee9649fae1bcc4f8c2
Danny <dannylaw06@gmail.com>
2023-05-28 20:28:53 +0100

    flower world gen

--- src/main/java/net/itsmythmaster/mccourse/world/ModWorldEvents.java
+++ src/main/java/net/itsmythmaster/mccourse/world/ModWorldEvents.java
@@ -1,6 +1,7 @@
 package net.itsmythmaster.mccourse.world;
 
 import net.itsmythmaster.mccourse.MCCourseMod;
+import net.itsmythmaster.mccourse.world.gen.ModFlowerGeneration;
 import net.itsmythmaster.mccourse.world.gen.ModTreeGeneration;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -9,7 +10,9 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = MCCourseMod.MOD_ID)
 public class ModWorldEvents {
     @SubscribeEvent
-    public static void biomeLoadingEvent(final BiomeLoadingEvent event) {
+    public static void biomeLoadingEvent(final BiomeLoadingEvent event)
+    {
         ModTreeGeneration.generateTrees(event);
+        ModFlowerGeneration.generateFlowers(event);
     }
 }
