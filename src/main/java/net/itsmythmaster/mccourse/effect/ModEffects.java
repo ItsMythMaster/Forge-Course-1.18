faa73e19cde8e1aa1c844983abc74d1676097e4c
Danny <dannylaw06@gmail.com>
2023-05-31 15:19:58 +0100

    freezing potion and effect fixes

--- src/main/java/net/itsmythmaster/mccourse/effect/ModEffects.java
+++ src/main/java/net/itsmythmaster/mccourse/effect/ModEffects.java
@@ -12,8 +12,8 @@ public class ModEffects {
     public static final DeferredRegister<MobEffect> MOB_EFFECTS
             = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, MCCourseMod.MOD_ID);
 
-    public static final RegistryObject<MobEffect> FREEZE = MOB_EFFECTS.register("freeze",
-            () -> new FreezeEffect(MobEffectCategory.HARMFUL, 3124687));
+    public static final RegistryObject<MobEffect> FREEZING = MOB_EFFECTS.register("freezing",
+            () -> new FreezingEffect(MobEffectCategory.HARMFUL, 3124687));
 
     public static void register(IEventBus eventBus) {
         MOB_EFFECTS.register(eventBus);
